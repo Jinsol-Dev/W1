9994543712698b1f4de629887d726a56936e4d6f
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-11-16 01:36:18 +0900

    관리자 contact us 구현

--- static/js/intro.js
+++ static/js/intro.js
@@ -1,9 +1,50 @@
 // 관리자 버튼
 const adminbtn = document.getElementById('admin-btn')
 const admincontent = document.querySelector('.admin-container')
-console.log(adminbtn)
+const questform = document.querySelector('.formbox')
 
 adminbtn.addEventListener('click', function (e) {
-  console.log(admincontent)
   admincontent.classList.toggle('admin-container')
+})
+
+const questHandler = () => {
+  admincontent.classList.toggle('admin-container')
+  questform.classList.toggle('formbox')
+}
+
+const jquest = document.getElementById('jquest')
+jquest.addEventListener('click', questHandler)
+const jsquest = document.getElementById('jsquest')
+jsquest.addEventListener('click', questHandler)
+const jbquest = document.getElementById('jbquest')
+jbquest.addEventListener('click', questHandler)
+const jhquest = document.getElementById('jhquest')
+jhquest.addEventListener('click', questHandler)
+
+const cancelquest = document.getElementById('buttonc')
+cancelquest.addEventListener('click', function () {
+  questform.classList.toggle('formbox')
+})
+
+// emailjs 라이브러리 사용
+const btn = document.getElementById('buttone')
+
+document.getElementById('form').addEventListener('submit', function (event) {
+  event.preventDefault()
+
+  btn.value = '보내는 중...'
+
+  const serviceID = 'bam'
+  const templateID = 'template_c7kl71g'
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = 'Send Email'
+      alert('문의되었습니다.')
+    },
+    (err) => {
+      btn.value = 'Send Email'
+      alert(JSON.stringify(err))
+    },
+  )
 })
